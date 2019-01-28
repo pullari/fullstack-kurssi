d4c9e07f97feb6494db932b5f89b23a69f3b42f5
samuli.rouvinen <samuli.rouvinen@cs.helsinki.fi>
2019-01-28 15:16:31 +0200

    Osa 1b tehty

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -14,35 +14,53 @@ const Part = (props) => (
 )
 
 const Content = (props) => { 
+
+  let names = props.parts.map(p => p.name)
+  let exercises = props.parts.map(p => p.exercises)
+
   return (
     <>
-      <Part part={props.parts[0]} exercise={props.exercises[0]} />
-      <Part part={props.parts[1]} exercise={props.exercises[1]} />
-      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+      <Part part={names[0]} exercise={exercises[0]} />
+      <Part part={names[1]} exercise={exercises[1]} />
+      <Part part={names[2]} exercise={exercises[2]} />
     </>
   )
 }
 
-const Total = (props) => (
+const Total = (props) => {
+  let total = props.parts.reduce((a, b) => a + b.exercises, 0);
+  
+  return (
     <p>
-        yhteensä {props.total} tehtävää
+      yhteensä {total} tehtävää
     </p>
-)
+  )
+}
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
